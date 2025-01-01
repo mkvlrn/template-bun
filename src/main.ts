3b533156b8040436e486ef6bb32562933d1b9246
Mike Valeriano <mkvlrn@proton.me>
2024-12-31 22:23:34 -0300

    feat: use biome, revamp a bit

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import { add } from "~/lib/basic-math";
 
 const twoPlusTwo = add(2, 2);
 
-// eslint-disable-next-line no-console
+// biome-ignore lint/suspicious/noConsoleLog: simple output
+// biome-ignore lint/suspicious/noConsole: simple output
 console.log(twoPlusTwo);
--- test/lib/advanced-math.test.ts
+++ test/lib/advanced-math.test.ts
@@ -1,22 +1,32 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, it } from "bun:test";
 import { divide, multiply } from "~/lib/advanced-math";
 
 describe("advanced math", () => {
-  test("multiply", () => {
-    const twoTimesTwo = multiply(2, 2);
-    const twoTimesThree = multiply(2, 3);
+  describe("multiply", () => {
+    it("should multiply two numbers", () => {
+      const twoTimesTwo = multiply(2, 2);
+      const twoTimesThree = multiply(2, 3);
 
-    expect(twoTimesTwo).toBe(4);
-    expect(twoTimesThree).not.toBe(4);
+      expect(twoTimesTwo).toBe(4);
+      expect(twoTimesThree).not.toBe(4);
+    });
   });
 
-  test("divide", () => {
-    const twoDividedByTwo = divide(2, 2);
-    const twoDividedByThree = divide(2, 3);
-    const divideByZero = () => divide(10, 0);
+  describe("divide", () => {
+    it("should divide two numbers", () => {
+      const twoDividedByTwo = divide(2, 2);
+      const twoDividedByThree = divide(2, 3);
+      const divideByZero = () => divide(10, 0);
 
-    expect(twoDividedByTwo).toBe(1);
-    expect(twoDividedByThree).not.toBe(1);
-    expect(divideByZero).toThrow("Division by zero");
+      expect(twoDividedByTwo).toBe(1);
+      expect(twoDividedByThree).not.toBe(1);
+      expect(divideByZero).toThrow("Division by zero");
+    });
+
+    it("should throw on divide by zero", () => {
+      const divideByZero = () => divide(10, 0);
+
+      expect(divideByZero).toThrow("Division by zero");
+    });
   });
 });
